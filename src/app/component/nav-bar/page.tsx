0281028197d6047f3b2d9b0bac276e05a8c0d98a
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2024-12-05 15:37:57 +0500

    update

--- src/app/component/nav-bar/page.tsx
+++ src/app/component/nav-bar/page.tsx
@@ -1,35 +1,106 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import { RiSearchLine } from "react-icons/ri";
-import { IoHeartOutline } from "react-icons/io5";
-import { IoCartOutline } from "react-icons/io5"
+import { IoHeartOutline, IoCartOutline } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <main>
-        <div className='flex justify-around h-20 items-center'>
-            {/* logo */}
-            <ul>
-                <li className='font-bold text-lg cursor-pointer'>Exclusive</li>
-            </ul>
-            {/* tabs */}
-            <ul className='flex gap-5'>
-                <li className='hover:underline hover:[text-decoration-color:grey] cursor-pointer'>Home</li>
-                <li  className='hover:underline hover:[text-decoration-color:grey] cursor-pointer'>Contact</li>
-                <li  className='hover:underline hover:[text-decoration-color:grey] cursor-pointer'>About</li>
-                <li  className='hover:underline hover:[text-decoration-color:grey] cursor-pointer'>Sign Up</li>
-            </ul>
-            {/* input ,search-icon, wishlist, cart */}
-            <ul className='flex gap-5 items-center'>
-             <div className='flex items-center bg-inputColor py-1 px-5 rounded-md'>
-             <li><input className='bg-inputColor outline-none placeholder:text-sm' type="text" placeholder='What are you looking for?'/></li>
-             <li><RiSearchLine className='size-5 cursor-pointer' /></li>
-             </div>
-                <li><IoHeartOutline className='size-5 cursor-pointer'/></li>
-                <li><IoCartOutline className='size-5 cursor-pointer'/></li>  
-            </ul>
-            
-        </div>
-    </main>
-  )
-}
+      <div className="flex justify-around h-20 items-center px-5 md:px-10">
+        {/* Logo */}
+        <ul>
+          <li className="font-bold text-lg cursor-pointer">Exclusive</li>
+        </ul>
 
-export default NavBar
+        {/* Tabs */}
+        <ul className="hidden md:flex gap-5">
+          <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+            Home
+          </li>
+          <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+            Contact
+          </li>
+          <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+            About
+          </li>
+          <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+            Sign Up
+          </li>
+        </ul>
+
+        {/* Input, Search Icon, Wishlist, Cart */}
+        <ul className="flex gap-5 items-center">
+          {/* Search Bar */}
+          <div className="flex items-center bg-inputColor py-1 px-5 rounded-md">
+            <li>
+              <input
+                className="bg-inputColor outline-none placeholder:text-sm"
+                type="text"
+                placeholder="What are you looking for?"
+              />
+            </li>
+            <li>
+              <RiSearchLine className="text-xl cursor-pointer" />
+            </li>
+          </div>
+
+          {/* Wishlist */}
+          <li>
+            <IoHeartOutline className="text-xl cursor-pointer hidden md:flex" />
+          </li>
+
+          {/* Cart */}
+          <li>
+            <IoCartOutline className="text-xl cursor-pointer hidden md:flex" />
+          </li>
+
+          {/* Hamburger Menu */}
+          <li className="md:hidden">
+            <RxHamburgerMenu
+              onClick={() => setOpen(!open)}
+              className="text-2xl cursor-pointer"
+            />
+          </li>
+        </ul>
+      </div>
+
+      {/* Mobile Menu */}
+      {open && (
+        <div className="md:hidden bg-inputColor p-5">
+          <div className="grid grid-cols-2">
+            <div>
+              <ul className="flex flex-col gap-4">
+                <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+                  Home
+                </li>
+                <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+                  Contact
+                </li>
+                <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+                  About
+                </li>
+              </ul>
+            </div>
+            {/* right grid */}
+            <div className="list-none flex flex-col items-end gap-5">
+              {" "}
+              <li>
+                <IoHeartOutline className="text-xl cursor-pointer " />
+              </li>
+              {/* Cart */}
+              <li>
+                <IoCartOutline className="text-xl cursor-pointer" />
+              </li>
+              <li className="hover:underline hover:[text-decoration-color:grey] cursor-pointer">
+                Sign Up
+              </li>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default NavBar;
